7d1fe2252b7a11715331c1be69557ad987e7c430
action0709 <action0709@mail.ru>
2023-05-14 01:55:40 +0400

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import org.w3c.dom.html.HTMLTableRowElement;
+
 public class Main {
+    private static final String REGEX = "^[a-zA-Z0-9_]*$";
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            check("action","12345","12345");
+            System.out.println("Проверка прошла успешно");
+        } catch (WrongLoginException|WrongPasswordException e) {
+            System.out.println(e.getMessage());
+        }
+        }
+        public static void check (String login, String password, String confirmPassword)
+            throws
+            WrongLoginException, WrongPasswordException{
+       if(!login.matches(REGEX)){
+           throw new WrongLoginException("В логине не допустимые символы");
+       }
+        if(login.length()>20){
+            throw new WrongLoginException("Размер логина превышает максимально допустимое значение");
+        }
+        if(!password.matches(REGEX)){
+            throw new WrongPasswordException("В пароле не допустимые символы");
+        }
+        if(password.length()>20){
+            throw new WrongPasswordException("Размер пароля превышает максимально допустимое значение");
+        }
+        if(!password.equals(confirmPassword)){
+            throw new WrongPasswordException("Пароли при проверке не совпадают");
+        }
     }
+
 }
---
+++ src/WrongLoginException.java
@@ -0,0 +1,18 @@
+public class WrongLoginException extends Exception {
+
+    public WrongLoginException(String message) {
+        super(message);
+    }
+
+    public WrongLoginException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public WrongLoginException(Throwable cause) {
+        super(cause);
+    }
+
+    public WrongLoginException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
